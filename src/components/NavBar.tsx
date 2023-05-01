98aa5068ee1effb7aa56fa431610c2aaf5c5270f
Anjan Rai <rai.unknown@gmail.com>
2023-05-01 23:37:23 +1000

    refactored code for sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,75 +1,80 @@
 import 'animate.css';
-import {FiMenu} from 'react-icons/fi';
-import {MdClose} from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
+// import {MdClose} from 'react-icons/md';
 import './NavBar.scss';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const NavBar = () => {
-    const links = [
-        {
-            linkTo: '/',
-            name: 'aboutMe',
-            hoverState: false,
-            linkTitle: 'About Me',
-            className: ''
-        },
-        {
-            linkTo: '/',
-            name: 'portfolio',
-            hoverState: false,
-            linkTitle: 'Portfolio',
-            className: ''
-        },
-        {
-            linkTo: '/',
-            name: 'resume',
-            hoverState: false,
-            linkTitle: 'Resume',
-            className: ''
-        },
-        {
-            linkTo: '/',
-            name: 'contactMe',
-            hoverState: false,
-            linkTitle: 'Contact Me',
-            className: ''
-        }
-    ]
-    
-    const handleHoverIn = (e: React.MouseEvent) => {
-        e.target.className = 'animate__animated animate__jello';
-        // console.log(e.target.id);
-        // links.filter(link => (link.name === e.target.id ? link.className = !link.hoverState: link.hoverState));
-    }
+  const links = [
+    {
+      linkTo: '/',
+      name: 'aboutMe',
+      hoverState: false,
+      linkTitle: 'About Me',
+      className: '',
+    },
+    {
+      linkTo: '/',
+      name: 'portfolio',
+      hoverState: false,
+      linkTitle: 'Portfolio',
+      className: '',
+    },
+    {
+      linkTo: '/',
+      name: 'resume',
+      hoverState: false,
+      linkTitle: 'Resume',
+      className: '',
+    },
+    {
+      linkTo: '/',
+      name: 'contactMe',
+      hoverState: false,
+      linkTitle: 'Contact Me',
+      className: '',
+    },
+  ];
 
-    const handleHoverOut = (e: React.MouseEvent) => {
-        e.target.className = '';
-    }
+  const handleHoverIn = (e: React.MouseEvent) => {
+    e.target.className = 'animate__animated animate__jello';
+    // console.log(e.target.id);
+    // links.filter(link => (link.name === e.target.id ? link.className = !link.hoverState: link.hoverState));
+  };
 
-    const [menuState, setMenuState] = useState(false);
+  const handleHoverOut = (e: React.MouseEvent) => {
+    e.target.className = '';
+  };
 
-    const handleIconClick = () => {
-        setMenuState(!menuState);     
-    }
-    
-    useEffect(() => {
-        menuState ? setBurgerNavlistClass('burger-navlist animate__animated animate__backInRight') : setBurgerNavlistClass('burger_navlist animate__animated animate__backOutRight')
-    }, [menuState]);
+  const [menuState, setMenuState] = useState(false);
 
-    const [burgerNavlistClass, setBurgerNavlistClass] = useState('burger-navlist');
-    
-    return <div className='navbar-wrapper'>
-        <ul className="navbar">
-            {links.map(link => (
-                <li  key={link.name}>
-                    <a id={link.name} href={link.linkTo} onMouseEnter={handleHoverIn} onMouseLeave={handleHoverOut}>{link.linkTitle}</a>
-                </li>
-            ))}
-        </ul>
-        <div className='burger-navbar'>
-            {menuState ? null: (<FiMenu className='icon' onClick={handleIconClick} />)}            
-        </div>
+  const handleIconClick = () => {
+    setMenuState(!menuState);
+  };
+
+  return (
+    <div className="navbar-wrapper">
+      <ul className="navbar">
+        {links.map((link) => (
+          <li key={link.name}>
+            <a
+              id={link.name}
+              href={link.linkTo}
+              onMouseEnter={handleHoverIn}
+              onMouseLeave={handleHoverOut}
+            >
+              {link.linkTitle}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="burger-navbar">
+        {menuState ? null : (
+          <FiMenu className="icon" onClick={handleIconClick} />
+        )}
+      </div>
     </div>
-}
+  );
+};
 
 export default NavBar;
