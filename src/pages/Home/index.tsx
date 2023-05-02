31d05e6d8df05aef90d77794042045310e68e85c
Anzanrai <rai.unknown@gmail.com>
2023-05-02 18:10:54 +1000

    navbar completed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     return <div className='home_container'>
         <NavBar />
         <div className='content-wrapper'>
-            <div className='child'>Here goes content</div>
+            <div className='child'>Here goes content for description</div>
             <div className='child right'>Here goes logo animation</div>
         </div>
     </div>
