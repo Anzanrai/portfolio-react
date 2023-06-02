f1d3c250a0f8cf85bb8529ac839c7c5ce2b8d371
Anzanrai <rai.unknown@gmail.com>
2023-06-02 11:05:55 +1000

    Added a summary and picture to the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,42 +1,31 @@
 import 'animate.css';
-import Typed from 'typed.js';
+import myPic from '../../assets/images/me.jpeg';
 
 import './index.scss';
 import NavBar from '../../components/NavBar';
 import Board from '../../components/Board';
-import { useEffect, useRef } from 'react';
+import TypedComponent from '../../components/TypedComponent';
 
 const Home = () => {
-    const el = useRef(null);
-    const typed = useRef(null);
-    useEffect(() => {
-        const options = {
-            strings: [
-            'Frontend Developer.',
-            'Backend Developer.',
-          ],
-          typeSpeed: 50,
-          backSpeed: 50,
-        }
-        typed.current = new Typed(el.current, options);
     
-        return () => {
-            // Make sure to destroy Typed instance during cleanup
-            // to prevent memory leaks
-            typed.current.destroy();
-        }
-    }, []);
-
+    const banners = ['A Frontend Developer.', 'A Backend Developer'];
+    const summary = ['I am a versatile developer proficient in frontend and backend technologies, with expertise in multiple programming languages. Skilled in HTML, CSS, and JavaScript, I create responsive user interfaces using frameworks like React, and Typescript. On the backend, I design RESTful APIs, handle data storage, and implement security measures. With a strong problem-solving ability, I troubleshoot and optimize code execution. I am experienced in agile environments, collaborating with cross-functional teams to deliver exceptional results. Proficient in communicating complex technical concepts, I am an effective team player. Adaptable to new technologies, my skills ensure the development of robust and user-friendly web applications.']
     
     return <div className='home_container'>
         <NavBar />
         <div className='content-wrapper'>
             <div className='child'>
-                <div className='child-content'>
-                    <span>I am</span>
+                <div className='child-image'>
+                    <img src={myPic} alt="me working" />
                 </div>
                 <div className='child-content'>
-                    <span className='variable-text' style={{whiteSpace: 'normal' }} ref={el} />
+                    <span>I am Anjan Rai.</span>
+                </div>
+                <div className='child-content'>
+                    <TypedComponent typedStrings={banners} typeSpeed={100} backSpeed={50} />
+                </div>
+                <div className='summary-content'>
+                    <TypedComponent typedStrings={summary} typeSpeed={10} backSpeed={50} />
                 </div>
             </div>
             <Board />
