ffc1b2c0f1a5a09777c25c2db5768aca0bbdc227
Anzanrai <rai.unknown@gmail.com>
2023-06-09 13:53:24 +1000

    removed summary from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,6 @@ import React from 'react';
 
 const Home = () => {
   const banners = ['A Frontend Developer.', 'A Backend Developer.'];
-  const summary ='I am a versatile developer proficient in frontend and backend technologies, with expertise in multiple programming languages. Skilled in HTML, CSS, and JavaScript, I create responsive user interfaces using frameworks like React, and Typescript. On the backend, I design RESTful APIs, handle data storage, and implement security measures. With a strong problem-solving ability, I troubleshoot and optimize code execution. I am experienced in agile environments, collaborating with cross-functional teams to deliver exceptional results. Proficient in communicating complex technical concepts, I am an effective team player. Adaptable to new technologies, my skills ensure the development of robust and user-friendly web applications.';
 
   return (
     <div className="home_container">
@@ -30,9 +29,7 @@ const Home = () => {
               backSpeed={50}
             />
           </div>
-          <div className="summary-content">
-            <p className='animate__animated animate__fadeInUp'>{summary}</p>
-          </div>
+          
         </div>
         <Board />
       </div>
