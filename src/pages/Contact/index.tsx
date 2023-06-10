e4d9d87859147e804318be9e21d520e4c73c7365
Anjan Rai <rai.unknown@gmail.com>
2023-06-10 14:10:04 +1000

    adding animated background to the contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,14 +1,20 @@
 import NavBar from '../../components/NavBar/';
 import SocialPanel from '../../components/SocialPanel';
 import Hire from '../../components/Hire';
+
+import './index.scss';
+import AnimatedBackground from '../../components/AnimatedBackground/index';
 function Contact() {
   return (
-    <div>
-      <NavBar />
-      Contact
-      <SocialPanel />
-      <Hire />
-    </div>
+    <>
+      <div className="contact-content-wrapper">
+        <NavBar />
+        Contact
+        <SocialPanel />
+        <Hire />
+      </div>
+      <AnimatedBackground />
+    </>
   );
 }
 
