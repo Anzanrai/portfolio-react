87171d9b0abd8b1f4468caceb1b7a9c8f2a727fe
Anjan Rai <rai.unknown@gmail.com>
2023-06-10 14:10:30 +1000

    added a new AnimatedBackground component

---
+++ src/components/AnimatedBackground/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.scss';
+
+const AnimatedBackground = () => {
+  return (
+    <div className="area">
+      <ul className="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default AnimatedBackground;
