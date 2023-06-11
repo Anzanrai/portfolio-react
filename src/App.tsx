b31573cfab104e3bdfc551e7e10bdd102286001e
Anjan Rai <rai.unknown@gmail.com>
2023-06-11 20:08:48 +1000

    added route and element for sanity admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import React from "react";
-
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Admin from './pages/Admin';
+import React from 'react';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>
+    path: '/',
+    element: <Home />,
   },
   {
-    path: "/projects",
-    element: <Projects />
+    path: '/projects',
+    element: <Projects />,
   },
   {
-    path: "/about-me",
-    element: <About />
+    path: '/about-me',
+    element: <About />,
   },
   {
-    path: "/contact",
-    element: <Contact />
-  }
+    path: '/contact',
+    element: <Contact />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+  },
 ]);
 
 function App() {
-
-  return (<>
-    <RouterProvider router={router}/>
-  </>
-    
-  )
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
