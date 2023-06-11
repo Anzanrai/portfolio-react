9a05fc969752c56c17b3251826ce04f8d0af4630
Anjan Rai <rai.unknown@gmail.com>
2023-06-11 20:09:05 +1000

    added admin page for sanity

---
+++ src/pages/Admin/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { NextStudio } from 'next-sanity/studio';
+import config from '../../../sanity.config';
+
+export default function Admin() {
+  return <NextStudio config={config} />;
+}
