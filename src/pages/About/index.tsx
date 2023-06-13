6efa4bee067f2b100c9562940a484b90602b59cd
Anjan Rai <rai.unknown@gmail.com>
2023-06-13 16:35:25 +1000

    added generic type array to initialise experiences and educations state variables

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -25,8 +25,8 @@ interface education {
 function Resume() {
   const summary =
     'I am a versatile developer proficient in frontend and backend technologies, with expertise in multiple programming languages. Skilled in HTML, CSS, and JavaScript, I create responsive user interfaces using frameworks like React, and Typescript. On the backend, I design RESTful APIs, handle data storage, and implement security measures. With a strong problem-solving ability, I troubleshoot and optimize code execution. I am experienced in agile environments, collaborating with cross-functional teams to deliver exceptional results. Proficient in communicating complex technical concepts, I am an effective team player. Adaptable to new technologies, my skills ensure the development of robust and user-friendly web applications.';
-  const [experiences, setExperiences] = useState([]);
-  const [educations, setEducations] = useState([]);
+  const [experiences, setExperiences] = useState<experience[]>([]);
+  const [educations, setEducations] = useState<education[]>([]);
 
   useEffect(() => {
     client
