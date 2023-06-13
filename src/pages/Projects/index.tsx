190c068b7a7b4777493c992f46390c566b9d22ff
Anjan Rai <rai.unknown@gmail.com>
2023-06-13 15:11:58 +1000

    retrieving projects from sanity

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../components/Card';
 import NavBar from '../../components/NavBar';
 import SocialPanel from '../../components/SocialPanel';
 import './index.scss';
-import FlappyImage from '../../assets/images/flappy-logo.png';
-import MuziclyImage from '../../assets/images/muzicly-logo.png';
 import Hire from '../../components/Hire/index';
+import { client } from '../../../sanity/sanity-utils';
 
 const Portfolio = () => {
-  const projects = [
-    {
-      imageLink: MuziclyImage,
-      projectTitle: 'Muzicly',
-      description:
-        'The Music Player App is a ReactJS and Redux-powered application similar to Spotify. It utilizes the ShazamCore API for retrieving music-related data, providing users with a seamless music streaming experience. Search for songs, create playlists, and enjoy uninterrupted playback in this dynamic and intuitive app.',
-      projectLink: 'https://muzicly.netlify.app/',
-    },
-    {
-      imageLink: FlappyImage,
-      projectTitle: 'Flappy Bird - Clone',
-      description:
-        'The Flappy Bird Clone project is a recreation of the popular mobile game using HTML, CSS, and JavaScript. It showcases my skills in front-end web development by combining structured HTML, visually appealing CSS, and interactive JavaScript to create a fun and engaging Flappy Bird clone that can be played directly in a web browser.',
-      projectLink: 'https://flappy-bird-app.netlify.app/',
-    },
-  ];
+  const [sanityProjects, setSanityProjects] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "project"]{
+        _id, _createdAt, projectTitle, description, "imageLink": imageLink.asset->url, url, projectLink
+      }`
+      )
+      .then((result: any) => {
+        setSanityProjects(result);
+      })
+      .catch((error: any) => console.log(error));
+    return () => {
+      console.log('Component will be unmount');
+    };
+  }, []);
+
+  console.log('Projects from sanity', sanityProjects);
+
   return (
     <>
       <NavBar />
@@ -47,7 +50,7 @@ const Portfolio = () => {
 
       <h2>My Projects</h2>
       <div className="card-container">
-        {projects.map(
+        {sanityProjects?.map(
           ({ imageLink, projectLink, projectTitle, description }, i) => (
             <Card
               key={i}
