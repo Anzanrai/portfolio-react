00130d05286ee327ede2ef24de574db7296126d5
Anjan Rai <rai.unknown@gmail.com>
2023-06-13 20:16:43 +1000

    using cdn for images

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -24,28 +24,28 @@ const Portfolio = () => {
       projectTitle: 'Muzicly',
       description:
         'The Music Player App is a ReactJS and Redux-powered application similar to Spotify. It utilizes the ShazamCore API for retrieving music-related data, providing users with a seamless music streaming experience. Search for songs, create playlists, and enjoy uninterrupted playback in this dynamic and intuitive app.',
-      imageLink: Muzicly,
+      imageLink: 'https://i.postimg.cc/0NqwK5Dw/muzicly-logo.png',
       projectLink: 'https://muzicly.netlify.app/',
     },
     {
       projectTitle: 'GlobalMedics',
       description:
         'GlobalMedics is a MERN stack project that facilitated remote communication between doctors and patients during COVID-19. It provided virtual appointments, medical information, and mental health support, improving healthcare accessibility.',
-      imageLink: GlobalMedics,
+      imageLink: 'https://i.postimg.cc/jjvGn5mg/globalmedics-logo.gif',
       projectLink: 'https://globalmedics.ai/',
     },
     {
       projectTitle: 'RevvedAI',
       description:
         'At Revved AI, I worked as a backend developer using MongoDB, Node.js, and Express.js to create an AI-based learning and coaching platform to help improve the learning procedure of students.',
-      imageLink: Revvedai,
+      imageLink: 'https://i.postimg.cc/VNMW2HBj/revved-ai-logo.png',
       projectLink: 'https://www.revved.ai/',
     },
     {
       projectTitle: 'Flappy Bird - Clone',
       description:
         'The Flappy Bird Clone project is a recreation of the popular mobile game using HTML, CSS, and JavaScript. It showcases my skills in front-end web development by combining structured HTML, visually appealing CSS, and interactive JavaScript to create a fun and engaging Flappy Bird clone that can be played directly in a web browser.',
-      imageLink: Flappy,
+      imageLink: 'https://i.postimg.cc/26tjvTMK/flappy-logo.png',
       projectLink: 'https://flappy-bird-app.netlify.app/',
     },
   ];
